1ad5ced7497d609ffc354c89e1fc7e1a678c1439
ftmanspile <codfan15@gmail.com>
2018-02-02 15:11:23 -0500

    added player 1 game

--- app.rb
+++ app.rb
@@ -2,7 +2,26 @@ require "sinatra"
 
 
 get '/' do
-	"hello world"
+	erb :home
 end
+
+post '/player_one' do
+	player_one_first_name = params[:fname]
+	player_one_last_name = params[:lname]
+	player_one_game_name = params[:gname]
+	redirect 'result?fname=' + player_one_first_name + '&lname=' + player_one_last_name + '&gname=' + player_one_game_name
+end
+
+get '/result' do
+	player_one_first_name = params[:fname]
+	player_one_last_name = params[:lname]
+	player_one_game_name = params[:gname]
+	erb :result, :locals => {:fname => player_one_first_name, :lname => player_one_last_name, :gname => player_one_game_name}
+end
+
+
+
+
+
 
 
