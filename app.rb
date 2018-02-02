bf3fb8a3461cbfee4fbb38e7624ba86ad16dd8ae
ftmanspile <codfan15@gmail.com>
2018-02-02 16:21:56 -0500

    finished and working

--- app.rb
+++ app.rb
@@ -19,6 +19,36 @@ get '/result' do
 	erb :result, :locals => {:fname => player_one_first_name, :lname => player_one_last_name, :gname => player_one_game_name}
 end
 
+post '/player_two' do
+	player_one_first_name = params[:fname]
+	player_one_last_name = params[:lname]
+	player_one_game_name = params[:gname]
+	player_two_first_name = params[:fname2]
+	player_two_last_name = params[:lname2]
+	player_two_game_name = params[:gname2]
+	redirect 'final?fname=' + player_one_first_name + '&lname=' + player_one_last_name + '&gname=' + player_one_game_name + '&fname2=' + player_two_first_name + '&lname2=' + player_two_last_name + '&gname2=' + player_two_game_name
+end
+
+get '/final' do
+	player_one_first_name = params[:fname]
+	player_one_last_name = params[:lname]
+	player_one_game_name = params[:gname]
+	player_two_first_name = params[:fname2]
+	player_two_last_name = params[:lname2]
+	player_two_game_name = params[:gname2]
+	erb :final, :locals => {:fname => player_one_first_name, :lname => player_one_last_name, :gname => player_one_game_name, :fname2 => player_two_first_name, :lname2 => player_two_last_name, :gname2 => player_two_game_name}
+end
+
+post '/last' do
+	player_one_first_name = params[:fname]
+	player_one_last_name = params[:lname]
+	player_one_game_name = params[:gname]
+	player_two_first_name = params[:fname2]
+	player_two_last_name = params[:lname2]
+	player_two_game_name = params[:gname2]
+end
+
+
 
 
 
